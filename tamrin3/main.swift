33ef59da5356df3a3ba8d059c55626b91bf24f0b
baharkhd <baharkh127@gmail.com>
2020-06-26 16:38:23 +0430

    Initial Commit

---
+++ tamrin3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  tamrin3
+//
+//  Created by Bahar Khodabakhshian on 4/6/1399 AP.
+//  Copyright © 1399 AP Bahar Khodabakhshian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
